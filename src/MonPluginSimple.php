10d5ef421bd2b51b6d768eda0820d265e93250e6
marwen <malyaoui@gmail.com>
2024-12-23 12:20:58 +0100

    First commit

---
+++ src/MonPluginSimple.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MonPluginSimple;
+class MonPluginSimple
+{
+    public function helloWorld()
+    {
+        return "Hello World! Ce plugin fonctionne avec Composer et PHP 5.6+.";
+    }
+
+}
